ae0bfefc47a5bc0a74167d7a9f52a0ebaf4803ae
ZackSaid17 <itszacksaid@gmail.com>
2024-05-01 09:30:07 +0100

    add html css js files

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+const display = document.getElementById('display');
+
+
+
+function appendToDisply(input){
+    display.value += input;
+}
+
+function clearValue(){
+
+
+
+    display.value = "";
+
+}
+
+
+function calculate(){
+
+    try{
+        display.value = eval(display.value);
+    }
+    catch(e){
+        display.value = "Error";
+    }
+
+    
+
+}
+
+
+
+
+
